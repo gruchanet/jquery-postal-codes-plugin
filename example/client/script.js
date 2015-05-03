ee14d7f16b725ed44e1a5dd48cb3890e745553db
gruchanet <grucha.net@gmail.com>
2015-05-03 21:11:40 +0200

    add validation option

--- example/client/script.js
+++ example/client/script.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
 
     $('#postalCode').postalCode({
         outputSelector: '#postalCity',
+        validate: true,
         url: '../server/postal_codes.php'
     });
 
--- postal-code.js
+++ postal-code.js
@@ -14,7 +14,8 @@
 
         // handling `key pressing` event
         $(this).keyup(function () {
-            var postalCode = $(this).val();
+            var postalInput = $(this);
+            var postalCode = postalInput.val();
             var isPostalCodeCorrect = postalCode.search(options.format) !== -1;
 
             if (isPostalCodeCorrect) {
@@ -25,13 +26,29 @@
                         data: { code: postalCode },
                         dataType: 'json',
                         success: function (data) {
-                            $(cityElem).val(data.city);
+                            // fill elem with city
+                            cityElem.val(data.city);
+
+                            if (options.validate) {
+                                // reset styles
+                                postalInput.css('border-color', '');
+                                // for label
+                                $('[for="' + postalInput.attr('id') + '"]').css('color', '');
+                            }
                         },
                         error: function () { // xhr
                             // TODO: error handling
                         }
                     });
                 }, options.delay);
+            } else {
+                if (options.validate) {
+                    // set error styles:
+                    // for input
+                    postalInput.css('border-color', '#a94442');
+                    // for label
+                    $('[for="' + postalInput.attr('id') + '"]').css('color', '#a94442');
+                }
             }
         });
     };
@@ -39,6 +56,7 @@
     $.fn.postalCode.defaults = {
         format: /\d{2}-\d{3}/i, // xx-xxx
         outputSelector: undefined,
+        validate: false,
         delay: 200,
         url: undefined
     };
